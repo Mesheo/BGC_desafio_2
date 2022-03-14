818af5b192faf7d4824f2980d5af5223c5240aaa
Michel Ledig <71408872+Mesheo@users.noreply.github.com>
2022-03-14 05:06:38 -0300

    Delete config.js

--- api/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user: "paiz2fyahoo@gmail.com",
-    client_id: "695846830448-jpqsuu3sirosl6vek1drtmfs282fsodj.apps.googleusercontent.com",
-    client_secret: "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",
-    refresh_token: "1//04tW0ZWSdxWHRCgYIARAAGAQSNwF-L9IryLlGv96KKQZ_kBV70ccV2A8ddIzOf_UFYJxlPgOftDuE0_DAZ3q0KhcBD6HncAtA66E"
-};
